662131b041299f956ed91d32a7856cc932a086e8
Lynwa <nicolas.tachet@gmail.com>
2023-03-06 13:28:01 +0100

    Update MainActivity.kt

--- Weather/app/src/main/java/master/kotlin/weather/MainActivity.kt
+++ Weather/app/src/main/java/master/kotlin/weather/MainActivity.kt
@@ -2,10 +2,22 @@ package master.kotlin.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import master.kotlin.weather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+        supportActionBar?.hide()
+        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this)
+        activityMainBinding.rlMainLayout.visibility = View.GONE
+
     }
 }
